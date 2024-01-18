68cb789b585d8d8f63f27cf70bd63d8b9171fe49
“Juan <jcrodgz@gmail.com>
2024-01-18 13:06:51 -0600

    first commit

---
+++ src/main/java/com/APICantabile/ApiCantabileApplication.java
@@ -0,0 +1,13 @@
+package com.APICantabile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCantabileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCantabileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/APICantabile/ApiCantabileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APICantabile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCantabileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
